341a574a9110889821086f1b4eb9b9be2c0df016
matteo <matteo.villa@chili.com>
2024-05-05 16:06:51 +0200

    [feat] delete directory if already exists

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -6,10 +6,11 @@ import { createDirectory } from "./utils";
 describe('createDirectory', () => {
   let existsSyncSpy: any;
   let mkdirSyncSpy: any;
+  let rmSyncSpy: any;
 
   beforeEach(() => {
     jest.resetModules(); // Resetta i moduli per assicurarti che gli spy siano puliti
-
+    rmSyncSpy = jest.spyOn(fs, 'rmSync').mockImplementation();
     mkdirSyncSpy =  jest.spyOn(fs, 'mkdirSync').mockImplementation();
   });
 
@@ -22,11 +23,13 @@ describe('createDirectory', () => {
     existsSyncSpy =  jest.spyOn(fs, 'existsSync').mockReturnValue(false);
     createDirectory("folderName");
     expect(mkdirSyncSpy).toHaveBeenCalledWith("folderName");
+    expect(rmSyncSpy).not.toHaveBeenCalled();
   });
 
   it("should not create a new directory if it does exist", () => {
     existsSyncSpy =  jest.spyOn(fs, 'existsSync').mockReturnValue(true);
     createDirectory("folderName");
     expect(mkdirSyncSpy).not.toHaveBeenCalledWith("folderName");
+    expect(rmSyncSpy).toHaveBeenCalled();
   });
 });
--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,7 @@ import fs from "fs";
 
 export const createDirectory = (dir: string) => {
     if (fs.existsSync(dir)) {
+      fs.rmSync(dir, { recursive: true, force: true });
       return;
     }
     fs.mkdirSync(dir);
